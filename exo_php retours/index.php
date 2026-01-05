b22975368332184a0aaebced03564888a1514d54
mizuchits <marin.tom65@gmail.com>
2026-01-05 11:31:49 +0100

    retours

---
+++ exo_php retours/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="http://localhost/exo%20PHP/exo_php%20retours/result.php?get=lien1">lien1</a>
+    <a href="http://localhost/exo%20PHP/exo_php%20retours/result.php?get=lien2">lien2</a>
+</body>
+</html>
---
+++ exo_php retours/login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="login.php" method="POST">
+        <input type="text" name="username" id="username">
+        <input type="text" name="password" id="password">
+        <input type="submit">
+    </form>
+
+    <?php
+    $user = [
+        "admin" => password_hash("admin", PASSWORD_DEFAULT),
+    ];
+    if (isset($_POST['username']) && ($_POST['password'])) {
+        if ($_POST['username'] == "admin" && $_POST['password'] == password_verify('admin', $user['admin'])) {
+            header('Location: login_success.php');
+            exit();
+        } else {
+            echo "<p>l'identifiant ou le mot de passe n'est pas correct</p>";
+        }
+    }
+    ?>
+</body>
+</html>
